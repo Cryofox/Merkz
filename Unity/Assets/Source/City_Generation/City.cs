3873ee6e5380d5df679dd970c5dd31a756f07ec8
Cryofox <hstances@uoguelph.ca>
2015-05-15 23:25:32 -0400

    Upgraded to unity 5...omg why did i wait for so long?

---
+++ Unity/Assets/Source/City_Generation/City.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class City : MonoBehaviour {
+	Maze maze;
+	// Use this for initialization
+	void Start () {
+		int dimensions= 51;
+		maze = new Maze(dimensions,dimensions);
+		DebugDraw_Grid(dimensions);
+	}
+	
+
+	public void DebugDraw_Grid(int dimensions)
+	{
+		float modif=10;
+		for(int x=0;x< 	dimensions;x++)
+		for(int y=0;y<	dimensions;y++)
+		{
+			Color color;
+			if(maze.blueprint[x][y]==1)
+				color= Color.red;
+			else if(maze.blueprint[x][y]==0)
+				color= Color.blue;
+			else if(maze.blueprint[x][y]==2)
+				color= Color.yellow;
+			else
+				color= Color.green;
+
+			if(color!=Color.red)
+			{
+				//Draw Top Line
+				Debug.DrawLine(new Vector3((float)(x*modif)-0.25f*modif,0f,(float)(y*modif)+0.25f *modif), new Vector3((float)(x*modif)+0.25f*modif,0f,(float)(y*modif)+0.25f*modif ),color,20);
+				//Draw Bot Line
+				Debug.DrawLine(new Vector3((float)(x*modif)-0.25f*modif,0f,(float)(y*modif)-0.25f *modif), new Vector3((float)(x*modif)+0.25f*modif,0f,(float)(y*modif)-0.25f*modif ),color,20);
+
+				//Draw Left Line
+				Debug.DrawLine(new Vector3((float)(x*modif)-0.25f*modif,0f,(float)(y*modif)-0.25f*modif ), new Vector3((float)(x*modif)-0.25f*modif,0f,(float)(y*modif)+0.25f*modif ),color,20);
+				//Draw Bot Line
+				Debug.DrawLine(new Vector3((float)(x*modif)+0.25f*modif,0f,(float)(y*modif)-0.25f*modif ), new Vector3((float)(x*modif)+0.25f*modif,0f,(float)(y*modif)+0.25f*modif ),color,20);				
+			}
+
+		}
+	}
+}
---
+++ Unity/Assets/Source/City_Generation/Maze.cs
@@ -0,0 +1,302 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class Node
+{
+	public int x,y;
+	public Node parent;
+	public List<Node> children; //List of Potential next nodes
+	public Node(int x, int y, Node parent)
+	{
+		children= new List<Node>();
+		this.parent=parent;
+		this.x=x;
+		this.y=y;
+	}
+	public void AddChild(Node node)
+	{
+		children.Add(node);
+	}
+
+}
+struct Point 
+{
+	public int x,y;
+	public Point(int x, int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+}
+
+	enum direction { Left, Right, Up, Down, None };
+
+public class Maze	
+{
+	//Legend
+	//0 = Pathway
+	//1 = Wall
+	//2 = Room
+	//3 = Door
+
+	public int[][] blueprint;
+	int maxW;
+	int maxH;
+	//Create a Maze with the Following Parameters
+	public Maze(int width, int height)
+	{
+		Random.seed=45;
+		//Setup Maze Dimensions
+		blueprint = new int[width][];
+
+		maxW= 	width;
+		maxH=	height;
+
+		for(int i=0;i<width;i++)
+		{
+			blueprint[i] = new int[height];
+			for(int j=0;j<height;j++)
+				blueprint[i][j]=1;
+		}
+
+		CreatePath();
+	}
+
+
+	void CreatePath()
+	{
+		//Method 1: Recursion/Subdivision
+		// int depth=0;
+		// int targetDepth=2;
+		// Node root = new Node(0,0, maxW-1,maxH-1);
+
+		// Subdivide(depth,targetDepth,root);
+
+		//Method 2: DFS
+		DFS();
+		int roomDepth=5;
+		Create_Rooms(5);
+	}
+
+	Node root;
+	void DFS()
+	{
+		//Choose Start
+		int x = Random.Range(0, maxW);
+		int y = Random.Range(0, maxH);
+
+		List<Node> openCells= new List<Node>();
+		openCells.Add( root=(new Node(x,y,null)));
+
+
+
+		Node currentNode;
+		AddToOpen(openCells,openCells[0]);
+		while(openCells.Count>0)
+		{
+			currentNode = openCells[ openCells.Count-1 ];
+			blueprint[currentNode.x][currentNode.y]=0;
+
+			//Check 
+			direction dir = GetNextPath(currentNode);
+			Node newNode_1;
+			Node newNode_2;
+			switch (dir)
+			{
+				case direction.Left:
+				//Set the Parent. Useful for backtracking.
+					blueprint[(currentNode.x-1)][(currentNode.y)]=0;
+					newNode_1 = new Node((currentNode.x-1), (currentNode.y),	currentNode);
+					newNode_2 = new Node((currentNode.x-2), (currentNode.y), 	newNode_1);
+					currentNode.AddChild(newNode_2);
+					AddToOpen(openCells,newNode_2); //Adds to the End of the List					
+				break;
+
+				case direction.Right:
+				//Set the Parent. Useful for backtracking.
+					blueprint[(currentNode.x+1)][(currentNode.y)]=0;
+					newNode_1 = new Node((currentNode.x+1), (currentNode.y),	currentNode);
+					newNode_2 = new Node((currentNode.x+2), (currentNode.y), 	newNode_1);
+
+					currentNode.AddChild(newNode_2);
+					AddToOpen(openCells,newNode_2); //Adds to the End of the List						
+				break;
+
+				case direction.Up:
+				//Set the Parent. Useful for backtracking.
+					blueprint[(currentNode.x)][(currentNode.y+1)]=0;
+					newNode_1 = new Node((currentNode.x), (currentNode.y+1),	currentNode);
+					newNode_2 = new Node((currentNode.x), (currentNode.y+2), 	newNode_1);
+
+					currentNode.AddChild(newNode_2);
+
+					AddToOpen(openCells,newNode_2); //Adds to the End of the List	
+						
+				break;
+
+
+				case direction.Down:
+				//Set the Parent. Useful for backtracking.
+					blueprint[(currentNode.x)][(currentNode.y-1)]=0;
+					newNode_1 = new Node((currentNode.x), (currentNode.y-1),	currentNode);
+					newNode_2 = new Node((currentNode.x), (currentNode.y-2),	newNode_1);
+
+					currentNode.AddChild(newNode_2);
+					AddToOpen(openCells,newNode_2); //Adds to the End of the List		
+				break;
+				//No Direction was found. BackTrack.
+				default:
+					// openCells.Add(currentNode.parent);
+					//No Adjacent Cells here. Remove it from open list
+					openCells.RemoveAt(openCells.Count-1);
+				break;
+			}
+		}
+	}
+
+	void Create_Rooms(int depth)
+	{
+		//Grab all Children
+		// starting at root
+		List<Node> leaves = new List<Node>();
+		List<Node> openList= new List<Node>();
+
+		openList.Add(root);
+		while(openList.Count>0)
+		{
+			if(openList[0].children.Count==0)
+			{
+				//A leaf has no children.
+				leaves.Add(openList[0]);
+			}
+			//Add all the children of this node to the openlist
+			for(int i=0;i< openList[0].children.Count;i++)
+			{
+				openList.Add(openList[0].children[i]);
+			}
+			//Remove the current node from open list
+			openList.RemoveAt(0);
+		}
+
+		for(int i=0; i< leaves.Count;i++)
+		{
+		//	blueprint[leaves[i].x][leaves[i].y]=2;
+			ExpandRoom(leaves[i],0,depth);
+		}
+
+		Room_Shapify();
+	}
+
+	//Given a leaf node it converts the walkable path into "rooms"
+	//this is done untill the depth set is reached
+	void ExpandRoom(Node currentNode, int curDepth, int targetDepth)
+	{
+		if(curDepth>=targetDepth)
+			return;
+		if(currentNode==null)
+			return;
+
+		blueprint[currentNode.x][currentNode.y]=2;
+		ExpandRoom( currentNode.parent, (curDepth+1), targetDepth);
+	}
+
+	//Now we Try to Approximate roomShape by converting the weird
+	//lines into containers
+	void Room_Shapify()
+	{
+		List<Point> roomPoints = new List<Point>();
+		//First get a list of all yellow points (this is needed so the new points dont get added to the equation)
+		for(int y=0;y<maxH;y++)		
+			for(int x=0;x<maxW;x++)
+			{
+				if(blueprint[x][y]==2)
+					roomPoints.Add( new Point(x,y));
+			}
+
+		//Now for each point in the list, add it's neighbour points to yellow (regardless if a path)
+		for(int i=0;i< roomPoints.Count;i++)
+		{
+			//up
+			if(roomPoints[i].y+1< maxH)
+				blueprint[ roomPoints[i].x][ roomPoints[i].y+1]=2;
+			//down
+			if(roomPoints[i].y-1>=0)
+				blueprint[ roomPoints[i].x][ roomPoints[i].y-1]=2;
+
+			//right
+			if(roomPoints[i].x+1< maxH)
+				blueprint[ roomPoints[i].x+1][ roomPoints[i].y]=2;
+			//left
+			if(roomPoints[i].x-1>=0)
+				blueprint[ roomPoints[i].x-1][ roomPoints[i].y]=2;
+
+
+		}
+
+
+		Cleanup_Points();
+
+
+	}
+
+	void Cleanup_Points()
+	{
+		//If a yellow point exists where 3 of it's neighbours aren't also yellow, convert it blue
+		for(int y=0;y<maxH;y++)		
+			for(int x=0;x<maxW;x++)
+			{
+				if(blueprint[x][y]==2)
+				{
+					int count=0;
+					if(x+1<maxW && blueprint[x+1][y]!=2)
+						count++;
+					if(x-1>=0 && blueprint[x-1][y]!=2)
+						count++;
+					if(y+1<maxH && blueprint[x][y+1]!=2)
+						count++;
+					if(y-1>=0 && blueprint[x][y-1]!=2)
+						count++;
+
+					if(count>=3)
+						blueprint[x][y]=1;
+				}
+			}		
+
+	}
+
+
+
+
+
+
+
+
+	void AddToOpen(List<Node> openCells, Node curNode)
+	{
+		openCells.Add(curNode);
+		blueprint[curNode.x][curNode.y]=0;
+	}
+
+	direction GetNextPath(Node curNode)
+	{
+		//Create a List of available Directions
+		List<direction> paths = new List<direction>();
+
+		if(curNode.x+2<maxW && blueprint[curNode.x+2][curNode.y]==1)
+			paths.Add(direction.Right);
+		if(curNode.x-2>=0 	&& blueprint[curNode.x-2][curNode.y]==1)
+			paths.Add(direction.Left);
+		if(curNode.y+2<maxH &&  blueprint[curNode.x][curNode.y+2]==1)
+			paths.Add(direction.Up);		
+		if(curNode.y-2>=0 	&& blueprint[curNode.x][curNode.y-2]==1)
+			paths.Add(direction.Down);
+
+		if(paths.Count>0)
+			return paths[ Random.Range(0,paths.Count)];
+		return direction.None;
+	}
+
+
+
+}
